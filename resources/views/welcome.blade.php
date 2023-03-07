5321f90544c4ba31d252b46228031a9512c0f822
anbulbule <abhishekbulbule1@gmail.com>
2023-03-07 16:31:26 +0530

    laravel deployment test

---
+++ resources/views/welcome.blade.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+</head>
+<body>
+    <div style="background-color: gray; width:100%;height:100vh; text-align:center; ">
+        <h2 style="padding-top:250px; color:red">Laravel Test</h2>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Veniam dolorum similique saepe eveniet exercitationem excepturi placeat. Facere, dicta autem. Quos pariatur accusamus molestias ex voluptate eaque recusandae modi maxime sunt.</p>
+    </div>
+</body>
+</html>
